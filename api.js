f8e512bf88d7db5a3f10e5061550b7899ee551dc
raofaeez <122422917+raofaeez@users.noreply.github.com>
2024-08-01 17:00:10 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,335 @@
+const items=[
+    {
+        id:'0',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'1',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'2',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'3',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'4',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'5',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'6',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'7',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'8',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'9',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'10',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'11',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'12',
+        name:"Men Zara",
+        img:"images/b1.jpg",
+        category:"men",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'13',
+        name:"Women Zara",
+        img:"images/g1.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'14',
+        name:"Men Zara",
+        img:"images/g2.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'15',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'16',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'17',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'18',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'19',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'20',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'21',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'22',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'23',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'24',
+        name:"Men Zara",
+        img:"images/g3.jpg",
+        category:"women",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'25',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'26',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'27',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'28',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'29',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'30',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'31',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'32',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'33',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'34',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'35',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    },
+    {
+        id:'36',
+        name:"kids Zara",
+        img:"images/k3.jpg",
+        category:"kids",
+        price:"200",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elite."
+
+    }
+]
